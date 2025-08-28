97b1be042a989fa6117c61cb894121b5081ee42a
Avinash Narain <aviapollyon@gmail.com>
2025-08-28 15:27:35 +0200

    Practice Ver 0.1

---
+++ Coding Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿PrintArray();
+
+void PrintArray()
+{
+    int age = 0;
+    bool validInput = false;
+
+    while (validInput == false)
+    {
+        Console.WriteLine("Please enter your age:");
+        string? input = Console.ReadLine();
+
+        if (input != null)
+        {
+            validInput = int.TryParse(input, out age);
+        }
+
+        Console.WriteLine("\n...Invalid Input...\n");
+        
+
+    }
+    int currentDate = DateTime.Now.Year;
+    int dateOfBirth = currentDate - age;
+    Console.WriteLine(dateOfBirth);
+};
