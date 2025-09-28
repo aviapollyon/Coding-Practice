39ea90b3d29bd8fe0901d6721f9cdfc9dc054812
Avinash Code <aviapollyon@gmail.com>
2025-09-28 07:30:38 +0200

    pullingPC

--- Coding Practice/Program.cs
+++ Coding Practice/Program.cs
@@ -1,31 +1,63 @@
-﻿string[,] corporate = 
+﻿using System;
+
+string[] pettingZoo = 
 {
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-string[,] external =
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6) 
 {
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
-
-string externalDomain = "@hayworth.com";
-
-displayEmail(corporate);
-displayEmail(external, externalDomain);
-
-void displayEmail(string[,] employeeList, string domain = "@contoso.com")
-{
-    
-    // display  email addresses
-    for (int i = 0; i < employeeList.GetLength(0); i++)
-    {
-        string username = (employeeList[i, 0].Substring(0, 2) + employeeList[i, 1]).ToLower();
-        Console.WriteLine(username + domain);
-    }
-
+    RandomizeAnimals(); 
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
 }
 
+void RandomizeAnimals() 
+{
+    Random random = new Random();
 
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
+void PrintGroup(string[,] groups) 
+{
+    for (int i = 0; i < groups.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < groups.GetLength(1); j++) 
+        {
+            Console.Write($"{groups[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
